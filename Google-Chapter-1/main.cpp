14d8f49a0887a07b411574a2dcf6c183bad25dda
KemCake <santos.remi@icloud.com>
2015-02-09 23:34:35 +0100

    Initial Commit

---
+++ Google-Chapter-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Google-Chapter-1
+//
+//  Created by Remi Santos on 09/02/15.
+//  Copyright (c) 2015 Google. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
